8658d6970357c0a50e2a171881982d799aaccda3
George <theonlygusti@gmail.com>
2018-11-08 08:03:27 +0000

    Delete blank line between imports
    Google Java Style Guide recommends keeping all non-static imports in
    one block without any blank lines separating them.

--- minecraft-server/ShowDamageAsLevel/src/main/java/io/github/theonlygusti/dmgindicator/EntityDamageByEntityHandler.java
+++ minecraft-server/ShowDamageAsLevel/src/main/java/io/github/theonlygusti/dmgindicator/EntityDamageByEntityHandler.java
@@ -1,7 +1,6 @@
 package io.github.theonlygusti.dmgindicator;
 
 import org.bukkit.entity.Player;
-
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
