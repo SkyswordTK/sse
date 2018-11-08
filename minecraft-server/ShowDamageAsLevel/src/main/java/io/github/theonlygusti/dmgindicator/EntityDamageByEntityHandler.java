9ee068fbb8bb80fb630bc794503b5084973d831e
George <theonlygusti@gmail.com>
2018-11-08 21:28:57 +0000

    Set handler priority to lowest

--- minecraft-server/ShowDamageAsLevel/src/main/java/io/github/theonlygusti/dmgindicator/EntityDamageByEntityHandler.java
+++ minecraft-server/ShowDamageAsLevel/src/main/java/io/github/theonlygusti/dmgindicator/EntityDamageByEntityHandler.java
@@ -2,11 +2,15 @@ package io.github.theonlygusti.dmgindicator;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class EntityDamageByEntityHandler implements Listener {
-  @EventHandler
+  // other plugins will use EntityDamageByEntityEvent to set custom damages,
+  // this plugin should have lowest priority to make sure it shows the actual
+  // damage dealt to the player (after other plugins have set it).
+  @EventHandler(priority = EventPriority.LOWEST)
   public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
     // it only makes sense for players to be shown the damage of their last attack
     if (event.getDamager() instanceof Player) {
