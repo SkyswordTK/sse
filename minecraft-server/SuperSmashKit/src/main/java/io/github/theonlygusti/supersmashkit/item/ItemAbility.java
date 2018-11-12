6629e7bbcba9640f1eb8b130158edbbc2c242760
George <theonlygusti@gmail.com>
2018-11-12 22:20:08 +0000

    Create first iteration of the ItemAbility class

---
+++ minecraft-server/SuperSmashKit/src/main/java/io/github/theonlygusti/supersmashkit/item/ItemAbility.java
@@ -0,0 +1,21 @@
+package io.github.theonlygusti.supersmashkit.item;
+
+import io.github.theonlygusti.supersmashkit.SuperSmashKit;
+
+import org.bukkit.Material;
+
+public interface ItemAbility {
+  public String getName();
+  public String getLore();
+  public SuperSmashKit getOwner();
+  public Material getMaterial();
+  public void punch();
+  public void rightClick();
+  /**
+   * @return  The milliseconds.
+   * @see     
+   */
+  public long getCooldownTime();
+  public long getLastTimeUsed();
+}
+
