ea0719e911a222c8adb35b6ba2c388da56d9ca42
George <theonlygusti@gmail.com>
2018-11-12 22:20:51 +0000

    Implement new SuperSmashKit methods

--- minecraft-server/SuperSmashKit/src/main/java/io/github/theonlygusti/supersmashkit/kit/SkeletonKit.java
+++ minecraft-server/SuperSmashKit/src/main/java/io/github/theonlygusti/supersmashkit/kit/SkeletonKit.java
@@ -1,6 +1,10 @@
 package io.github.theonlygusti.supersmashkit.kit;
 
 import io.github.theonlygusti.supersmashkit.SuperSmashKit;
+import io.github.theonlygusti.supersmashkit.item.ItemAbility;
+
+import java.util.Arrays;
+import java.util.List;
 
 import me.libraryaddict.disguise.disguisetypes.Disguise;
 import me.libraryaddict.disguise.disguisetypes.DisguiseType;
@@ -8,20 +12,84 @@ import me.libraryaddict.disguise.disguisetypes.MobDisguise;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
 public class SkeletonKit implements SuperSmashKit {
   private Player player;
+  private BoneExplosion boneExplosion;
 
   public SkeletonKit(Player player) {
     this.player = player;
+    this.boneExplosion = new BoneExplosion(this);
+  }
+
+  public Player getPlayer() {
+    return this.player;
+  }
+
+  private class BoneExplosion implements ItemAbility {
+    private long lastTimeUsed = System.currentTimeMillis() - this.getCooldownTime();
+    private SkeletonKit owner;
+
+    public BoneExplosion(SkeletonKit owner) {
+      this.owner = owner;
+    }
+
+    public String getName() {
+      return "Bone Explosion";
+    }
+
+    public String getLore() {
+      return "";
+    }
+
+    public SuperSmashKit getOwner() {
+      return this.owner;
+    }
+
+    public Material getMaterial() {
+      return Material.IRON_AXE;
+    }
+
+    public void punch() {
+    }
+
+    public void rightClick() {
+      if (System.currentTimeMillis() - this.lastTimeUsed < this.getCooldownTime()) {
+        this.owner.getPlayer().sendMessage("The skill is not cooled down yet");
+      } else {
+        this.owner.getPlayer().sendMessage("You used the skill");
+        this.lastTimeUsed = System.currentTimeMillis();
+      }
+    }
+
+    public long getCooldownTime() {
+      return 10000L;
+    }
+
+    public long getLastTimeUsed() {
+      return this.lastTimeUsed;
+    }
+  }
+
+  public List<ItemAbility> getItemAbilities() {
+    return Arrays.asList(this.boneExplosion);
+  }
+
+  public void doPunch() {
+  }
+
+  public void doRightClick() {
+    int slot = this.player.getInventory().getHeldItemSlot();
+    if (slot < this.getItemAbilities().size()) {
+      this.getItemAbilities().get(slot).rightClick();
+    }
   }
 
   public Vector getDoubleJumpVelocity() {
-    // testing: horizontal double jump distance is about 6.334
-    //          vertical double jump height is about 6.40872
     Location playerLocation = this.player.getLocation();
 
     float playerPitch = playerLocation.getPitch();
@@ -30,11 +98,8 @@ public class SkeletonKit implements SuperSmashKit {
 
     Location newPitchLocation = playerLocation.clone();
     newPitchLocation.setPitch(newPitch);
-
     Vector pseudoDirection = newPitchLocation.getDirection();
-
     Vector velocity = pseudoDirection.multiply(0.9);
-
     return velocity;
   }
 
