00f93c11630725e52a753663a9160bd03cf05dc7
George <theonlygusti@gmail.com>
2018-11-12 16:33:20 +0000

    Add SuperSmashKit#getPlayer method

--- minecraft-server/SuperSmashKit/src/main/java/io/github/theonlygusti/supersmashkit/SuperSmashKit.java
+++ minecraft-server/SuperSmashKit/src/main/java/io/github/theonlygusti/supersmashkit/SuperSmashKit.java
@@ -7,11 +7,13 @@ import java.util.List;
 
 import me.libraryaddict.disguise.disguisetypes.Disguise;
 
+import org.bukkit.entity.Player;
 //import org.bukkit.inventory.ItemStack;
 
 public interface SuperSmashKit extends DoubleJumper {
   public Disguise getDisguise();
   public List<ItemAbility> getItemAbilities();
+  public Player getPlayer();
   //public List<PassiveAbility> getPassives();
   //public ItemStack[] getArmorContents();
 }
