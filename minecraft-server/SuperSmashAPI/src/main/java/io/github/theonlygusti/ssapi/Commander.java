1d2d76f5b5b86a6e80512d4fd70559b308d6edb2
George <theonlygusti@gmail.com>
2018-11-14 03:25:00 +0000

    Let the command set non-senders' kits

--- minecraft-server/SuperSmashAPI/src/main/java/io/github/theonlygusti/ssapi/Commander.java
+++ minecraft-server/SuperSmashAPI/src/main/java/io/github/theonlygusti/ssapi/Commander.java
@@ -8,15 +8,18 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class Commander implements CommandExecutor {
-  public Commander() {
+  private Plugin plugin;
+
+  public Commander(Plugin plugin) {
+    this.plugin = plugin;
   }
 
   @Override
   public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
     if (cmd.getName().equals("kit")) {
-      if (sender instanceof Player) {
-        Player player = (Player) sender;
-        if (args.length == 1) {
+      if (args.length == 1) {
+        if (sender instanceof Player) {
+          Player player = (Player) sender;
           if (player.hasPermission("ssapi.kitcmd.self")) {
             if (!SuperSmashController.exists(args[0])) {
               player.sendMessage("§cThe §f§l"  + args[0] + "§r §ckit has not been registered§r");
@@ -31,12 +34,34 @@ public class Commander implements CommandExecutor {
             return true;
           }
         } else {
-          return false;
+          sender.sendMessage("You must be a player to use this command.");
+
+          return true;
+        }
+      } else if (args.length == 2) {
+        if (sender.hasPermission("ssapi.kitcmd.other")) {
+          if (!SuperSmashController.exists(args[0])) {
+            sender.sendMessage("§cThe §f§l"  + args[0] + "§r §ckit has not been registered§r");
+            return true;
+          }
+
+          Player target = this.plugin.getServer().getPlayerExact(args[1]);
+
+          if (target == null) {
+            sender.sendMessage("§cThe player with username " + args[1] + " could not be found on the server§r");
+            return true;
+          }
+          
+          SuperSmashController.enkit(target, args[0]);
+          sender.sendMessage("§r§e§l" + args[1] + "§r §ahas been given the §r§e§l" + args[0] + "§r §akit§r");
+          target.sendMessage("§aYou have been given the §r§e§l" + args[0] + "§r §akit§r");
+          return true;
+        } else {
+          sender.sendMessage("§cYou do not have permission to give other players a kit with this command§r");
+          return true;
         }
       } else {
-        sender.sendMessage("You must be a player to use this command.");
-
-        return true;
+        return false;
       }
     } else if (cmd.getName().equals("dekit")) {
       if (sender instanceof Player) {
