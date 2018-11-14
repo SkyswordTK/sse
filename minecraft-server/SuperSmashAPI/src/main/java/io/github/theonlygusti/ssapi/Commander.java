f7c5c183091af301a4d30fa91f042c004ac8f7e4
George <theonlygusti@gmail.com>
2018-11-14 02:58:51 +0000

    Refactor to allow arguments to be added

--- minecraft-server/SuperSmashAPI/src/main/java/io/github/theonlygusti/ssapi/Commander.java
+++ minecraft-server/SuperSmashAPI/src/main/java/io/github/theonlygusti/ssapi/Commander.java
@@ -15,21 +15,21 @@ public class Commander implements CommandExecutor {
   public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
     if (cmd.getName().equals("kit")) {
       if (sender instanceof Player) {
-        if (args.length != 1) {
+        if (args.length == 1) {
+          if (!SuperSmashController.exists(args[0])) {
+            sender.sendMessage("§cThat kit is not registered§r");
+            return true;
+          }
+
+          Player player = (Player) sender;
+
+          SuperSmashController.enkit(player, args[0]);
+          sender.sendMessage("§aYou have been given the §r§e§l" + args[0] + "§r §akit§r");
+
+          return true;
+        } else {
           return false;
         }
-
-        if (!SuperSmashController.exists(args[0])) {
-          sender.sendMessage("§cThat kit is not registered§r");
-          return true;
-        }
-
-        Player player = (Player) sender;
-
-        SuperSmashController.enkit(player, args[0]);
-        sender.sendMessage("§aYou have been given the §r§e§l" + args[0] + "§r §akit§r");
-
-        return true;
       } else {
         sender.sendMessage("You must be a player to use this command.");
 
@@ -37,21 +37,21 @@ public class Commander implements CommandExecutor {
       }
     } else if (cmd.getName().equals("dekit")) {
       if (sender instanceof Player) {
-        if (args.length != 0) {
+        if (args.length == 0) {
+          Player player = (Player) sender;
+
+          if (!SuperSmashController.isKitted(player)) {
+            sender.sendMessage("§cYou do not have a kit§r");
+            return true;
+          }
+
+          SuperSmashController.dekit(player);
+          sender.sendMessage("§cYou no longer have a kit§r");
+
+          return true;
+        } else {
           return false;
         }
-
-        Player player = (Player) sender;
-
-        if (!SuperSmashController.isKitted(player)) {
-          sender.sendMessage("§cYou do not have a kit§r");
-          return true;
-        }
-
-        SuperSmashController.dekit(player);
-        sender.sendMessage("§cYou no longer have a kit§r");
-
-        return true;
       } else {
         sender.sendMessage("You must be a player to use this command.");
 
