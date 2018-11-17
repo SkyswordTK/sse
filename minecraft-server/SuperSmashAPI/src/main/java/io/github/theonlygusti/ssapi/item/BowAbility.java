9d8c1f550e350f309a8744397524fa65a0c6350b
George <theonlygusti@gmail.com>
2018-11-17 11:17:04 +0000

    Extend ItemAbility in BowAbility

--- minecraft-server/SuperSmashAPI/src/main/java/io/github/theonlygusti/ssapi/item/BowAbility.java
+++ minecraft-server/SuperSmashAPI/src/main/java/io/github/theonlygusti/ssapi/item/BowAbility.java
@@ -2,7 +2,7 @@ package io.github.theonlygusti.ssapi.item;
 
 import org.bukkit.entity.Arrow;
 
-public interface BowAbility {
+public interface BowAbility extends ItemAbility {
   public void onShootArrow(Arrow arrow);
 }
 
