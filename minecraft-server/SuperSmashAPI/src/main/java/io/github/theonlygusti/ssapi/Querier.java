219babe632de7982516dfabd990f13fb08b3ee98
George <theonlygusti@gmail.com>
2018-11-18 15:09:28 +0000

    Add command to debug memory leaks

--- minecraft-server/SuperSmashAPI/src/main/java/io/github/theonlygusti/ssapi/Querier.java
+++ minecraft-server/SuperSmashAPI/src/main/java/io/github/theonlygusti/ssapi/Querier.java
@@ -1,6 +1,7 @@
 package io.github.theonlygusti.ssapi;
 
 import io.github.theonlygusti.ssapi.SuperSmashController;
+import io.github.theonlygusti.ssapi.passive.Passive;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -16,6 +17,20 @@ public class Querier implements CommandExecutor {
   @Override
   public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
     if (cmd.getName().equals("ssc")) {
+      if (args.length >= 1) {
+        if (args[0].equals("passives")) {
+          for (Passive passive : SuperSmashController.getRunningPassives()) {
+            sender.sendMessage(passive.getName());
+            sender.sendMessage(passive.getOwner().getPlayer().getName());
+          }
+        } else if (args[0].equals("huh")) {
+          return false;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
     }
     return false;
   }
